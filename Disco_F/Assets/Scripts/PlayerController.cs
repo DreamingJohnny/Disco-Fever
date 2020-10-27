4a33df0c28a7b2dc157bc8272493f4862012a113
Wiive <wirrechan@hotmail.com>
2020-10-27 13:47:49 +0100

    Added Boss Material

--- Disco_F/Assets/Scripts/PlayerController.cs
+++ Disco_F/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Vector3 movement = new Vector3();
-    public float moveDistance = 0.5f;
+    float moveDistance = 0.5f;
     bool movingRight;
-    RaycastHit2D hit;
+
+
 
     void Start()
     {
         movement = GetComponent<Transform>().position;
-
     }
 
 
-    //Working to see if I can make a Raycast from player to the collider
-    private void FixedUpdate()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics2D.Raycast(transform.position, -Vector3.up))
-        {
-            Debug.DrawLine(ray.origin, hit.point);
-        }
-    }
     void Update()
     {
+        //RaycastHit2D hit;
+        //float theDistance = 0.25;
+
+        //Vector3 right = transform.TransformDirection(Vector3.right) * 0.25f;
+        
+        
         float x = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Horizontal"))
@@ -51,7 +48,6 @@ public class PlayerController : MonoBehaviour
         else
             movement = new Vector3(0,0);
         
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -71,5 +67,6 @@ public class PlayerController : MonoBehaviour
             movement.y += (moveDistance / 2);
             transform.position += movement;
         }
+        movement = new Vector3(0, 0);
     }
 }
