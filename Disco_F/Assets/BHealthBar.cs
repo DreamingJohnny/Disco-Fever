27e76e8ea479e3c8ebda0388048d6fd9dd06b592
Emilie <emivog0223@skola.goteborg.se>
2020-10-27 16:46:09 +0100

    Added boss health bar

---
+++ Disco_F/Assets/BHealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BHealthBar : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public BHealthBar BhealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        BhealthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        BhealthBar.SetHealth(currentHealth);
+    }
+}
+
