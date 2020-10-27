025e54039a89a0d6a97163cc5cc374cda103193a
Wiive <wirrechan@hotmail.com>
2020-10-27 10:47:45 +0100

    Added Player and player movement

---
+++ Disco_F/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 movement = new Vector3();
+    public float moveDistance = 0.5f;
+
+    void Start()
+    {
+        movement = GetComponent<Transform>().position;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            Debug.Log("Pressed the horizontal key");
+            if(x > 0)
+            {
+                movement.x += moveDistance;
+                movement.y += (moveDistance / 2);
+            }
+            else if(x < 0)
+            {
+                movement.x -= moveDistance;
+                movement.y -= (moveDistance / 2);
+            }
+
+            transform.position += movement;
+        }
+
+        else
+            movement = new Vector3(0,0);
+        
+
+    }
+}
