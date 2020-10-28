62fe085ab9a241b878647d7a0673595337ca89ae
Wiive <wirrechan@hotmail.com>
2020-10-28 15:19:09 +0100

    Created BossDummy

---
+++ Disco_F/Assets/Scripts/TestRemoveOnZeroHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRemoveOnZeroHealth : MonoBehaviour
+{
+    BossHealthBar bossHealth;
+
+    private void Start()
+    {
+        bossHealth = GetComponent<BossHealthBar>();
+    }
+
+    private void Update()
+    {
+        if (bossHealth.currentHealth <=  0)
+        {
+            Debug.Log("Try to die");
+            Destroy(gameObject);
+        }
+    }
+}
