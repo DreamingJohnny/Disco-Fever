f17729a24c4194a597803a4bd713475260f50165
Emilie <emivog0223@skola.goteborg.se>
2020-10-28 15:22:23 +0100

    v_03

--- Disco_F/Assets/Assets_DevilsWorkShop/NewBehaviourScript.cs
+++ Disco_F/Assets/Assets_DevilsWorkShop/NewBehaviourScript.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class IsometricCharacterRenderer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        public float movementSpeed = 1f;
+    IsometricCharacterRenderer isoRenderer;
+
+    Rigidbody2D rbody;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = horizontalInput.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Timeout.fixedDeltaTime;
+        isoRenderer.SetDirection(movement);
+        rbody.MovePosition(newPos);
     }
 }
+*/
---
+++ Disco_F/Assets/BossMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 bossVelocity;
+    private bool groundedBoss;
+    private float bossSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedBoss = controller.isGrounded;
+        if (groundedBoss && bossVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (Input.GetButtonDown("Jump") && groundedBoss)
+        {
+            bossVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        bossVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(bossVelocity * Time.deltaTime);
+    }
+}
