6376b7ea4b545d0a82842d1a5c50e558dcd5450a
Wiive <wirrechan@hotmail.com>
2020-10-29 11:11:21 +0100

    Updated NPCs and Player

--- Disco_F/Assets/Scripts/NPCHealthState.cs
+++ Disco_F/Assets/Scripts/NPCHealthState.cs
@@ -6,65 +6,58 @@ using UnityEngine;
 public class NPCHealthState : MonoBehaviour
 {
     //Logic operators for npc health and what state they are in
-    public float npcHealth;
+    float npcCurrentHealth;
     public float npcMaxHealth = 100;
     public float npcFeverThreshold = 10;
     public float npcCuredThreshold = 90;
+    public GameObject sicknessArea;
 
     //Enum to check state if npc is cured, neutral or feverish
-    enum npcCuredOrFeverish
+    enum npcState
     {
         isCured,
         isFeverish,
         isNeutral
     }
-    npcCuredOrFeverish npcHealthState;
-
-    //Gives each npc their own physical body that will affect others.
-    SicknessAura contagiousArea;
+    npcState npcCurrentState;
 
     void Start()
     {
-        npcHealthState = npcCuredOrFeverish.isNeutral;
+        npcCurrentState = npcState.isNeutral;
 
-        npcHealth = npcMaxHealth * 0.5f;
-
-        contagiousArea = new SicknessAura();
-        
-        //GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        //sphere.transform.position = new Vector3(0, 0, 0);
+        npcCurrentHealth = npcMaxHealth * 0.5f;
 
     }
 
     void Update()
     {
-        if (npcHealthState != npcCuredOrFeverish.isCured)
+        if (npcCurrentState != npcState.isCured)
         {
             NPCCheckHealth();
             //Need to increase their sickness aura here? Will boss also have sickness aura true?
         }
-        //else
-        //{
+        else 
+        {
             //if the NPC is cured, their aura needs to disappear,
 
-            //contagiousArea = 
-        //}
+            sicknessArea.SetActive(false);
+        }
     }
 
     public void NPCCheckHealth()
     {
 
-        if (npcHealth > npcCuredThreshold)
+        if (npcCurrentHealth > npcCuredThreshold)
         {
-            npcHealthState = npcCuredOrFeverish.isCured;
+            npcCurrentState = npcState.isCured;
         }
-        else if (npcHealth > npcFeverThreshold)
+        else if (npcCurrentHealth > npcFeverThreshold)
         {
-            npcHealthState = npcCuredOrFeverish.isNeutral;
+            npcCurrentState = npcState.isNeutral;
         }
-        else if (npcHealth < npcFeverThreshold)
+        else if (npcCurrentHealth < npcFeverThreshold)
         {
-            npcHealthState = npcCuredOrFeverish.isFeverish;
+            npcCurrentState = npcState.isFeverish;
         }
     }
 
@@ -73,11 +66,15 @@ public class NPCHealthState : MonoBehaviour
         /*The NPC health will only be affected if it is between this span,
         This essentially means that an NPC will be "innoculated" if their health ever is more than 90
         It also means that they cannot become "more sick" once their health has reached 0*/
-        if (npcHealth > 0 && npcHealthState != npcCuredOrFeverish.isFeverish)
+        if (npcCurrentHealth > 0 && npcCurrentState != npcState.isFeverish)
         {
-            npcHealth -= auraStrength * Time.deltaTime;
+            npcCurrentHealth -= auraStrength * Time.deltaTime;
         }
     }
 
+    public void TakeDamge(int damage)
+    {
+        npcCurrentHealth += damage;
+    }
 }
 
