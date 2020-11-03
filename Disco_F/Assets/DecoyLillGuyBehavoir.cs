8c32f6c6f2c67239afc073bc503a4c79569e4b3b
Wiive <wirrechan@hotmail.com>
2020-11-03 17:02:01 +0100

    Updated Assignment16 Meny Animation

---
+++ Disco_F/Assets/DecoyLillGuyBehavoir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoyLillGuyBehavoir : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("Hit");
+    }
+}
